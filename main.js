33e420267c7cd791bdf597046ab0ab81d8e44cd6
Brandon Wegrowski <bwegrowski@gmail.com>
2024-02-25 12:04:01 -0700

    Adding HTML, CSS, and JS for Add to Itinerary pop

--- main.js
+++ main.js
@@ -239,3 +239,21 @@ document.addEventListener('DOMContentLoaded', function() {
   removeButton.addEventListener('click', removeUsernameFromList);
 });
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-button")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Optional: Close the modal if the user clicks anywhere outside of the modal content
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
