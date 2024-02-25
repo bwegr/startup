f4549afe1c57bfbf26362062e0065228f4280488
Brandon Wegrowski <bwegrowski@gmail.com>
2024-02-25 12:14:42 -0700

    Pop-up success

--- main.js
+++ main.js
@@ -237,24 +237,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
   addButton.addEventListener('click', addUsernameToList);
   removeButton.addEventListener('click', removeUsernameFromList);
-
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-button")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// Optional: Close the modal if the user clicks anywhere outside of the modal content
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("addToI");
+  var span = document.getElementsByClassName("close-button")[0];
+
+  btn.onclick = function() {
+      modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+});
+
+
